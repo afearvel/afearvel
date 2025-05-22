df7a66fef090ea104171db0fb1c9c877e973e937
afearvel <cloudafe@gmail.com>
2025-05-21 22:51:26 -0600

    Add files via upload

---
+++ practica2-3.py
@@ -0,0 +1,21 @@
+from math import pi, tan
+
+class Figura:
+    
+    def areaFig(ladoLargo, numLado):
+        area = (1 / 4) * numLado * (ladoLargo ** 2) / tan(pi / numLado)
+        return area
+
+    def perimetroFig(ladoLargo, numLado):
+        perim = ladoLargo * numLado
+        return perim
+
+numLado = int(input("ingresa la cantidad de lados en la figura: "))
+ladoLargo = float(input("ingresa la medida del lado: "))
+
+if numLado <= 2 or ladoLargo <=0:
+    print("Figura no valida")
+else:
+    print(f"area de la figura: {Figura.areaFig(ladoLargo, numLado)}")
+    print(f"perímetro de la figura: {Figura.perimetroFig(ladoLargo, numLado)}")
+

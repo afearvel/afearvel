703cf62636f80b1de2b5e737a5081dd54709127c
afearvel <cloudafe@gmail.com>
2025-05-21 22:53:24 -0600

    Update practica4.py

--- practica4.py
+++ practica4.py
@@ -1,6 +1,6 @@
 class CuentaBancaria:
     def __init__(self, saldo):
-        self.__saldo = saldo  # Atributo privado
+        self.__saldo = saldo
 
     def mostrar_saldo(self):
         print(f"Saldo: {self.__saldo}")
@@ -9,5 +9,4 @@ class CuentaBancaria:
         self.__saldo += cantidad
 
 mi_cuenta = CuentaBancaria(100)
-# print(mi_cuenta.__saldo)
 mi_cuenta.mostrar_saldo() 
